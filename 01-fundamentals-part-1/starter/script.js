d83faa227035494c5629b7d62cf338d52e2b69b7
Jamaine Tuazon <tuazonjamaine12@gmail.com>
2025-08-20 13:02:14 +0800

    saved changes

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,73 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log("SAAN MOO SIYA DALAHIN");
+
+console.log("=== VARIABLES ===");
+
+let firstName = "Jonas";
+console.log(firstName)
+
+let age = 30;
+age = 31; // we can change Let variables
+console.log(age);
+
+const birthYear = 1991; 
+//birthYear = 2004; //WRONG ! const canot change value
+console.log(birthYear);
+
+const PI = 3.1415;
+console.log(PI);
+
+var job = "programmer";
+job = "teacher";
+console.log(job); 
+//modern js uses let and const, avoid using it
+
+age = 51;
+
+let lastName = "Doe";
+
+//do not dp this
+
+/*best practices: 
+use const by default
+use let only when you need to change the value*/
+
+console.log("=== DATA TYPES ===");
+
+//NUMBER
+age =24;
+console.log(age);
+console.log(typeof age);
+
+//STRING
+let username = "mark";
+console.log(username);
+console.log(typeof username);
+
+//BOOLEAN
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+
+//UNDEFINED
+let year;
+console.log(year);
+console.log(typeof year);
+
+//DYNAMIC VARIABLE
+let dynamicVariable = 23;
+console.log(dynamicVariable, typeof dynamicVariable);
+
+//changed into string
+dynamicVariable = "Now I'm a string";
+console.log(dynamicVariable, typeof dynamicVariable);
+
+//changed into a boolean
+dynamicVariable = true;
+console.log(dynamicVariable, typeof dynamicVariable);
+
+
+
